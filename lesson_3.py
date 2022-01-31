6183681082dfea8a645a7eb97f560ad7ad754e8c
TaisaKh <taisa.khranovska@gmail.com>
2022-01-31 13:46:41 +0200

    Add HW1 and HW2

---
+++ lesson_3.py
@@ -0,0 +1,39 @@
+# x = input("Enter: ")
+# if x == 5:
+#     print("hello")
+# else:
+#     x = input("Enter: ")
+#     if x == 5:
+#         print("hello")
+#     else:
+#         x = input("Enter: ")
+#         if x == 5:
+#             print("hello")
+
+#WHILE
+    #O(N^2)   <- вложеність цикла \/ 2-два цикла
+    #x = input("Enter: ")
+    # while x != 5:
+    #     x = input("Enter: ")  #body
+    #     while x != 5:
+    #         x = input("Enter: ")
+    #
+    # print("Hello")
+# x = "Hello"
+# size = len(x)
+# idx = 0
+# while idx != size:
+#     tmp = (x[idx])
+#     print(tmp)
+#     idx +=1
+# print("End")
+
+# x = "Hello"
+# for item in x:
+#     print(item)
+#
+# r = range(0, len(x))
+# for idx in r:
+#     print(x[idx])
+# print("End")
+
